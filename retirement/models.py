d2c975695e369865ebd0458cd5d4fa2315bce70b
notmanav <notmanav@github.com>
2018-04-25 14:21:18 -0700

    Perf Improvements in model

--- retirement/models.py
+++ retirement/models.py
@@ -1,11 +1,23 @@
 import uuid
+import logging
 from django.db import models
 from django.urls.base import reverse
-from retirement.utils import DateUtil
+from retirement.utils import DateUtil, MoneyUtil
+from retirement.utils import MoneyUtil
+from django.db.models import Q
+from _collections import defaultdict
+
+
+logger = logging.getLogger('django')
+moneyutil=MoneyUtil()
+TARGET_CURRENCY='USD'
 
 class Result(models.Model):
-    id=models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Result Id(Servers no purpose other then letting the Analysis transaction complete before doing the calculations")
+    id=models.AutoField(primary_key=True, help_text="Result Id(Servers no purpose other then letting the Analysis transaction complete before doing the calculations")
     analysis=models.OneToOneField('Analysis',on_delete=models.CASCADE)
+    
+    interest_dates_cache=defaultdict(list)
+    moneyutil=MoneyUtil()
     
     def save(self, *args, **kwargs):
         self.delete_old_transactions()
@@ -15,35 +27,114 @@ class Result(models.Model):
     def __str__(self):
         return (self.analysis.title + "-Results")
     
+    
+    def is_interest_day(self,related_asset,txDate):
+        dates=self.interest_dates_cache.get(related_asset.id)
+        if(dates==None or len(dates)==0):
+            dates=list()
+            dates.extend(related_asset.calc_next_x_interest_days(related_asset.acquire_date,10))
+            self.interest_dates_cache[related_asset.id]=dates
+        elif(dates[-1] < txDate):            
+            dates.extend(related_asset.calc_next_x_interest_days(dates[-1],len(dates)))
+        if txDate in dates:
+            return True
+        else:
+            return False
+            
+        
     def create_transactions(self):
         dateutility=DateUtil()
         #print(Asset.objects.filter(analysis__id=self.analysis.id))
         related_assets=Asset.objects.filter(analysis__id=self.analysis.id)
+        for related_asset in related_assets:
+            related_asset.prep_for_txn()
+            related_asset.save()
+            
         current_tx_date=self.analysis.start_date
+        
+        asset_instances_of_id=dict()
+        related_assets=Asset.objects.filter(analysis__id=self.analysis.id)
+        for related_asset in related_assets:        
+            asset_instances_of_id[related_asset.id]=AssetInstance.objects.filter(asset__id=related_asset.id)
         while(current_tx_date<=self.analysis.end_date):
+            #related_assets=Asset.objects.filter(analysis__id=self.analysis.id)
             for related_asset in related_assets:
-                related_asset.prep_for_txn()
-                if(related_asset.is_interest_day(current_tx_date)):
+                if(self.is_interest_day(related_asset,current_tx_date) and related_asset.txtype=='CREDIT'):
                     new_conservative_amount=related_asset.get_next_conservative_interest_amount()
-                    tx=Transaction()
-                    tx.analysis=self.analysis
-                    tx.txDate=current_tx_date
-                    tx.oldVal=related_asset.conservative_balance
-                    tx.newVal=new_conservative_amount
-                    related_asset.conservative_balance=new_conservative_amount
-                    tx.description="An interest "+related_asset.txtype +" of "+ str(tx.newVal-tx.oldVal) +" for "+related_asset.name + " on "+str(tx.txDate)+" for a total new balance of "+str(tx.newVal)
-                    tx.save()
-                for asset_instance in AssetInstance.objects.filter(asset__id=related_asset.id):
-                    if(asset_instance.txDate==current_tx_date):
+                    if(new_conservative_amount>0):
                         tx=Transaction()
                         tx.analysis=self.analysis
-                        tx.txDate=asset_instance.txDate
+                        tx.txDate=current_tx_date
                         tx.oldVal=related_asset.conservative_balance
-                        tx.newVal=tx.oldVal+asset_instance.txAmountMin
-                        related_asset.conservative_balance=tx.newVal
-                        tx.description="A "+related_asset.txtype +" of "+ str(asset_instance.txAmountMin) +" for "+related_asset.name + " on "+str(tx.txDate) +" for a total new balance of "+str(tx.newVal)
+                        tx.newVal=new_conservative_amount
+                        tx.txtype=related_asset.txtype
+                        tx.txcause='interest'
+                        tx.causing_asset_id=related_asset.id
+                        tx.impacted_asset_id=related_asset.id
+                        related_asset.conservative_balance=new_conservative_amount
+                        tx.description="An interest "+related_asset.txtype +" of "+ str(round(tx.newVal-tx.oldVal,2)) +" for "+related_asset.name + " on "+str(tx.txDate)+" for a total new balance of "+str(round(tx.newVal,2))
                         tx.save()
+                        related_asset.save()
+                for asset_instance in asset_instances_of_id.get(related_asset.id):
+                    if(asset_instance.txDate==current_tx_date):
+                        if(related_asset.txtype=='CREDIT'):
+                            tx=Transaction()
+                            tx.analysis=self.analysis
+                            tx.txDate=asset_instance.txDate
+                            tx.oldVal=related_asset.conservative_balance
+                            tx.newVal=tx.oldVal+asset_instance.txAmountMin
+                            tx.txtype='CREDIT'
+                            tx.txcause='recurring asset'
+                            tx.causing_asset_id=related_asset.id
+                            tx.impacted_asset_id=related_asset.id
+                            related_asset.conservative_balance=tx.newVal
+                            tx.description="A "+related_asset.txtype +" of "+ str(round(asset_instance.txAmountMin,2)) +" for "+related_asset.name + " on "+str(tx.txDate) +" for a total new balance of "+str(tx.newVal)
+                            tx.save()
+                            related_asset.save()
+                        else:
+                            # A super conservative approach will be to reduce liberal expenses from conservative assets but let's stick to conservative or liberal for both to have a realistic dataset
+                            self.reduce_next_most_liquid_asset_value(asset_instance.txAmountMax,related_asset.currency ,asset_instance.txDate,related_asset.name,related_asset.id)
             current_tx_date=dateutility.add_days(current_tx_date, 1)
+    
+    def reduce_next_most_liquid_asset_value(self,value, currency, txDate, asset_name, asset_id):
+        #get the next most liquid asset and create a new debit transaction on that asset;recursively
+        next_most_liquid_asset=Asset.objects.filter(~Q(conservative_balance=0.0)).order_by('-liquidity','-conservative_balance')[0]
+        if(next_most_liquid_asset==None):
+            logger.debug('No more money left. Crapping out... on '+str(txDate))
+            return
+        value_in_new_currency=moneyutil.convertAmount(value, currency, next_most_liquid_asset.currency)
+        if(next_most_liquid_asset.conservative_balance>value_in_new_currency):
+            tx=Transaction()
+            tx.analysis=self.analysis
+            tx.txDate=txDate
+            tx.oldVal=next_most_liquid_asset.conservative_balance
+            next_most_liquid_asset.conservative_balance-=value_in_new_currency
+            tx.newVal=next_most_liquid_asset.conservative_balance
+            tx.txtype='DEBIT'
+            tx.txcause='adjustment against an expense'
+            tx.causing_asset_id=asset_id
+            tx.impacted_asset_id=next_most_liquid_asset.id
+            tx.description="A Debit of "+ str(round(value_in_new_currency,2)) +" from "+next_most_liquid_asset.name + " due to "+ asset_name+" on "+str(txDate) +" for a total new balance of "+str(tx.newVal)
+            tx.save()
+            next_most_liquid_asset.save()
+        else:
+            next_most_liquid_asset_in_new_currency=moneyutil.convertAmount(next_most_liquid_asset.conservative_balance,next_most_liquid_asset.currency,currency)
+            tx=Transaction()
+            tx.analysis=self.analysis
+            tx.txDate=txDate
+            tx.oldVal=next_most_liquid_asset.conservative_balance
+            next_most_liquid_asset.conservative_balance=0.0
+            tx.newVal=0.0
+            tx.txtype='DEBIT'
+            tx.txcause='adjustment against an expense'
+            tx.causing_asset_id=asset_id
+            tx.impacted_asset_id=next_most_liquid_asset.id
+            tx.description="A Debit of "+ str(round(tx.oldVal,2)) +" from "+next_most_liquid_asset.name + " due to  "+ asset_name +" on "+str(txDate) +" for a total new balance of "+str(tx.newVal)
+            tx.save()
+            next_most_liquid_asset.save()
+            self.reduce_next_most_liquid_asset_value(value-next_most_liquid_asset_in_new_currency,txDate,asset_name)
+
+        
     
     def delete_old_transactions(self):
         transactions=Transaction.objects.filter(analysis__id=self.analysis.id)
@@ -74,47 +165,6 @@ class Analysis(models.Model):
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-    
-
-class Transaction(models.Model):
-    id=models.AutoField(primary_key=True, help_text="Transaction Id")
-    txDate=models.DateField(null=False)
-    description=models.CharField(max_length=500)
-    oldVal=models.FloatField(null=False)
-    newVal=models.FloatField(null=False) 
-    analysis=models.ForeignKey('Analysis',on_delete=models.CASCADE, help_text="All transactions in the analysis")
-    create_date=models.DateField(null=False, auto_now_add=True)
-    update_date=models.DateField(null=False, auto_now=True)
-
-    class Meta:
-        verbose_name_plural="Transactions"
-        
-    def __str__(self):
-        return self.description +" || "+str(self.oldVal)+ " || "+str(self.newVal)
-    
-    
-
-class AssetInstance(models.Model):
-    id=models.AutoField(primary_key=True, help_text="Asset Instance Id")
-    asset=models.ForeignKey('Asset',on_delete=models.CASCADE, help_text="Refer to the asset that created it")
-    txAmountMin=models.IntegerField(default=0)
-    txAmountMax=models.IntegerField(default=0)
-    txDate=models.DateField(null=False)
-    create_date=models.DateField(null=False, auto_now_add=True)
-    update_date=models.DateField(null=False, auto_now=True)
-    
-    class Meta:
-        verbose_name_plural="Asset Instances"
-        ordering = ['txDate']
-    
-    def __str__(self):
-        return self.asset.name + " ("+ str(self.txDate) +") - "+str(self.asset.currency)+" "+str(self.txAmountMin) +"/"+str(self.txAmountMax)
-    
-    def get_absolute_url(self):
-        """
-        Returns the url to access a particular instance of the model.
-        """
-        return reverse('asset-inst-detail', args=[str(self.id)])
 
 class Asset(models.Model):
     
@@ -148,7 +198,7 @@ class Asset(models.Model):
         (50,'FLUID')
     )
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique Asset Identifier")
+    id = models.AutoField(primary_key=True, help_text="Unique Asset Identifier")
     #analysis=models.ManyToManyField(Analysis,help_text="Add the analysis to an asset. Remember, results are per analysis")
     name=models.CharField(max_length=100, help_text="Enter the uniquely identifiable name of the asset")
     description=models.TextField(max_length=1000, null=True, help_text=" Asset Description")
@@ -167,14 +217,15 @@ class Asset(models.Model):
     interest_recurrence_conservative_growth_rate=models.FloatField(null=False, default=2.0)
     interest_recurrence_liberal_growth_rate=models.FloatField(null=False, default=6.0)
     
+    
+    conservative_balance=models.FloatField(null=False, default=0.0)
+    liberal_balance=models.FloatField(null=False, default=0.0)
+    
     active=models.BooleanField(null=False, default=True)
     create_date=models.DateField(null=False, auto_now_add=True)
     update_date=models.DateField(null=False, auto_now=True)
     
-    next_interest_day=None
     dateutil=DateUtil()
-    conservative_balance=0.0
-    liberal_balance=0.0
     
     
     def save(self, *args, **kwargs):
@@ -206,9 +257,8 @@ class Asset(models.Model):
 
     def prep_for_txn(self):
         self.dateutil=DateUtil()
-        # self.conservative_balance=0.0
-        # self.liberal_balance=0.0
-        self.next_interest_day=self.get_next_interest_date(self.acquire_date)
+        self.conservative_balance=0.0
+        self.liberal_balance=0.0
     
     
     def get_next_asset_instance_date(self,recurrence_number):
@@ -241,16 +291,6 @@ class Asset(models.Model):
             return self.dateutil.add_years(self.acquire_date.day,current_interest_date,1)
         return self.dateutil.add_years(self.acquire_date.day,current_interest_date,1) #assume annual as the de-facto
     
-    #Stateless. Does not set the next interest date by default. Call set_next_interest_day to change tp the next interest day
-    def is_interest_day(self,thedate):
-        if(thedate==self.next_interest_day):
-            return True
-        return False
-    
-    #Stateful. Every call increments the date to next occurrence
-    def set_next_interest_day(self):
-        self.next_interest_day=self.get_next_interest_date(self.next_interest_day)
-    
     def __str__(self):
         return self.name
     
@@ -259,3 +299,55 @@ class Asset(models.Model):
         Returns the url to access a particular instance of the model.
         """
         return reverse('asset-detail', args=[str(self.id)])
+    
+    def calc_next_x_interest_days(self, current_interest_date=acquire_date,x=10):
+        dates=list()
+        for i in range(x):
+            next_date=self.get_next_interest_date(current_interest_date)
+            dates.append(next_date)
+            current_interest_date=next_date
+            i+=1
+        return dates
+        
+
+class AssetInstance(models.Model):
+    id=models.AutoField(primary_key=True, help_text="Asset Instance Id")
+    asset=models.ForeignKey('Asset',on_delete=models.CASCADE, help_text="Refer to the asset that created it")
+    txAmountMin=models.IntegerField(default=0)
+    txAmountMax=models.IntegerField(default=0)
+    txDate=models.DateField(null=False)
+    create_date=models.DateField(null=False, auto_now_add=True)
+    update_date=models.DateField(null=False, auto_now=True)
+    
+    class Meta:
+        verbose_name_plural="Asset Instances"
+        ordering = ['txDate']
+    
+    def __str__(self):
+        return self.asset.name + " ("+ str(self.txDate) +") - "+str(self.asset.currency)+" "+str(self.txAmountMin) +"/"+str(self.txAmountMax)
+    
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of the model.
+        """
+        return reverse('asset-inst-detail', args=[str(self.id)])    
+
+class Transaction(models.Model):
+    id=models.AutoField(primary_key=True, help_text="Transaction Id")
+    txDate=models.DateField(null=False)
+    description=models.CharField(max_length=500)
+    oldVal=models.FloatField(null=False)
+    newVal=models.FloatField(null=False) 
+    analysis=models.ForeignKey('Analysis',on_delete=models.CASCADE, help_text="All transactions in the analysis")
+    txtype=models.CharField(max_length=10, null=False, choices=Asset.TxType, default='DEBIT')
+    txcause=models.CharField(max_length=10, null=False, default='INTEREST')
+    causing_asset_id=models.IntegerField(default=0, help_text="Enter the uniquely identifiable name of the asset due to which money is being transacted")
+    impacted_asset_id=models.IntegerField(default=0,  help_text="Enter the uniquely identifiable name of the asset to which money is being transacted")
+    create_date=models.DateField(null=False, auto_now_add=True)
+    update_date=models.DateField(null=False, auto_now=True)
+
+    class Meta:
+        verbose_name_plural="Transactions"
+        
+    def __str__(self):
+        return self.description +" || "+str(round(self.oldVal,2))+ " || "+str(round(self.newVal,2))
